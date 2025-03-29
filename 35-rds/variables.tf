1e4e441b02bbf686453cadc3d9e54e052e880a46
MMahiketh <99901318+MMahiketh@users.noreply.github.com>
2025-03-30 00:05:17 +0530

    Created rds infra

---
+++ 35-rds/variables.tf
@@ -0,0 +1,31 @@
+variable "project" {
+  type    = string
+  default = "expense"
+}
+
+variable "environment" {
+  type    = string
+  default = "dev"
+}
+
+variable "instance" {
+  type    = string
+  default = "mysql"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "db.t4g.micro"
+}
+
+# tags
+variable "rds_tags" {
+  type    = map(any)
+  default = {}
+}
+
+#Domain name for route 53 records
+variable "domain_name" {
+  type    = string
+  default = "mahdo.site"
+}
